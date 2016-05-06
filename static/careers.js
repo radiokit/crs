2fc65d98424e994e4bbd3e246b1efab6dc61bbc5
piotrekzyla <kontakt@piotrzyla.pl>
2016-05-06 15:52:27 +0200

    RBSG-4 quickfix hardcopying static folder

---
+++ static/careers.js
@@ -0,0 +1,13 @@
+import React, {
+  PropTypes,
+} from 'react';
+
+
+//import './contact.scss';
+const StaticTemplate = (props) => (
+  <div>
+    hello
+  </div>
+);
+
+export default StaticTemplate;
